9f13ea49f0cf13413c0a56ae6a82b7a0c19dc2ec
齐袁媛 <qiyuanyuan@qiyuanyuandeMacBook-Pro.local>
2021-09-08 11:21:32 +0800

    Files added

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="title"></h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>
